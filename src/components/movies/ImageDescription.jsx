069511e94585fa146e02637947e2ca91fedda401
stevencueto <steven.cueto1305@gmail.com>
2022-03-31 01:41:16 -0400

    more stlyes TODO: Add movies to playlist and movie info

---
+++ src/components/movies/ImageDescription.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageDescription = (props) => {
+  return (
+    <div>ImageDescription</div>
+  )
+}
+
+export default ImageDescription
--- src/components/movies/SingleMovie.jsx
+++ src/components/movies/SingleMovie.jsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom'
 import './ShowMovies/movies.css'
 import  fakePoster from '../../images/not-movie-pic.jpeg'
-
 const SingleMovie = (props) => {
   const imgPath = `http://image.tmdb.org/t/p/w780${props.movie.poster_path}`
   const img = !!props.movie.poster_path ? imgPath : fakePoster
-
+  const handleClick = () => {
+    console.log('click')
+  }
   return (
-    <div key={props.movie._id}>
-        <h5 className='small-movie-title'>{props.movie.title ||props.movie.name }</h5>
-        <Link to={`/movie/${props.movie.id}`}><img className='poster' src={img} alt="" /></Link>
+    <div key={props.movie._id} className="max-width">
+        <img onClick={()=> handleClick()} className='poster' src={img} alt="" />
     </div>
   )
 }
