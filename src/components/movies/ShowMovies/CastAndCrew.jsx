560c1874f7acc48081ec5dbc1f62296856c32e58
stevencueto <steven.cueto1305@gmail.com>
2022-04-03 06:11:03 -0400

    more styling

--- src/components/movies/ShowMovies/CastAndCrew.jsx
+++ src/components/movies/ShowMovies/CastAndCrew.jsx
@@ -3,12 +3,19 @@ import Crew from "./Crew"
 const CastAndCrew = (props) => {
   return (
     <section className='cast-grid'>
-        <div className="small-croll">
+      <div className="cast-cont">
+      <h3>Cast</h3>
+      <div className="small-croll">
             { props.cast.length > 0 ? props.cast.map( (cast) => { return <Cast key={cast.credit_id} cast={cast} ></Cast> }) : null}
-        </div>
+      </div>
+      </div>
+      <div className="cast-cont">
+        <h3>Cast</h3>
         <div className="small-croll">
             { props.crew.length > 0 ? props.crew.map( (crew) => { return <Crew key={crew.credit_id} crew={crew} ></Crew> }) : null}
         </div>
+        </div>
+       
 	</section>
 
   )
